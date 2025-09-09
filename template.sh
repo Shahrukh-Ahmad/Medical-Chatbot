fd134ddd1efd05f93547d137108000c39e6aa703
Shahrukh <shahcsevira@gmail.com>
2025-09-09 23:35:06 +0530

    first commit

---
+++ template.sh
@@ -0,0 +1,19 @@
+## creating directory
+
+mkdir -p src
+mkdir -p research
+
+
+## creating Files
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompts.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trial.ipynb
+touch requirements.txt
+
+echo "Directory and files created successfully"
+
